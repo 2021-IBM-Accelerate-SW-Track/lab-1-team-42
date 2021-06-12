8d01a23cb6ecb27ed1d8272143da73cc875cad73
CatMusty <jorji@mymail.mines.edu>
2021-06-11 19:02:47 -0500

    Test file altered, test input started

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   return (
     <div className="App">
     <Header/>
+    <h1>To Do List</h1>
+    <p>
+      TEST INPUT AREA <br></br>
+      <input type="text" id="userInput" placeholder="To Do..."></input>
+    </p>
     </div>
   );
 }
