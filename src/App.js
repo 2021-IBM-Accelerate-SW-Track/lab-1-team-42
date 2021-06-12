22a1df899a732cfc20ba0d2e9941473f31c92d74
CatMusty <jorji@mymail.mines.edu>
2021-06-11 19:50:56 -0500

    Background colour changed, inputField file created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Header from "./component/header"
 import React from 'react'
 import './App.css';
-// Test comment
+import inputField from './inputField.js';
+
 function App() {
+  
   return (
     <div className="App">
     <Header/>
     <h1>To Do List</h1>
-    <p>
-      TEST INPUT AREA <br></br>
-      <input type="text" id="userInput" placeholder="To Do..."></input>
-    </p>
+    <body>
+      
+    </body>
     </div>
   );
 }
-
 export default App;
