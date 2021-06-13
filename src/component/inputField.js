a58d06abefdba5ce7168d2877f93039a8e1a3f43
CatMusty <jorji@mymail.mines.edu>
2021-06-13 18:42:56 -0500

    App.js refactored for no hooks

--- src/component/inputField.js
+++ src/component/inputField.js
@@ -1,11 +1,20 @@
 import TextField from '@material-ui/core/TextField';
 
-export default function Field(){
+let target = "";
+function setTarget(item){
+    target = item;
+}
+
+export function getTarget(){
+    return target;
+}
+export function Field(){
     return(
         <TextField
             id="userInput" 
             label=""
             variant="filled"
+            onChange={(e) => setTarget(e.target.value)}
         />
     )
 }
