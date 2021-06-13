b45be05c366c9a801debada8d62ca6a275b50166
CatMusty <jorji@mymail.mines.edu>
2021-06-13 18:53:22 -0500

    list.js created and imports listed

---
+++ src/component/list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+// The following imports are optional and more can be found at
+// https://material-ui.com/components/lists. They might be good 
+// choices for whomever will be working on these facets of the
+// list functionality.
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CommentIcon from '@material-ui/icons/Comment';
