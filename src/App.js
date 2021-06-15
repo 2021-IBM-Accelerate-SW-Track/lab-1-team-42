ff43d38700fdb9254b1773adc2f83dfedecd03fb
CatMusty <jorji@mymail.mines.edu>
2021-06-14 19:55:49 -0500

    Revert "Add items to list, display them"
    This reverts commit 7d2f11104c835f83c3232ab20b9b9d096512d554.

--- src/App.js
+++ src/App.js
@@ -4,37 +4,23 @@ import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
+import display from './component/displayList';
+// import Grid from '@material-ui/core/Grid';
 
 function App() {
   // Hook to store the state of the textField, and
-  // a function to handle it.
+  // a function to handle it (stub for now).
   const [item, setItem] = useState('');
   
   let list = [];
   const handleItem = (e) =>{
     e.preventDefault()
 
-    if(item && !list.includes(item)){
+    if(item){
       list.push(item);
-      displayItem();
     }
   }
 
-  // displayItem
-  //
-  // // Adds to and displays the given list in order
-  function displayItem(){
-    // HTML list is identified by ID from the document
-    let displayList = document.getElementById('toDo');
-    // For each entry in the array, a new HTML child is
-    // created and appended to the HTML list, meaning every
-    // time this is called the display list is overwritten
-    for(let i = 0; i < list.length; i++){
-      let item = document.createElement('li');
-      item.appendChild(document.createTextNode(list[i]));
-      displayList.append(item);
-    }
-  }
 
   return (
     <div className="App">
@@ -43,26 +29,25 @@ function App() {
     {/* Body contains text field and button within the same form,
     which then allows for submission upon click.*/}
     <body>
-      <form noValidate autoComplete="off" onSubmit={handleItem}> 
+      <form noValidate autoComplete="off" onSubmit={handleItem}>
         <TextField
         onChange={(e) => setItem(e.target.value)} 
         id="userInput" 
         label="" 
-        variant="filled"
+        variant="filled" 
+        
         />
         <Button          // FIXME add space b/w button and field
-          type="submit"
+          type="submit" 
           variant="contained" 
           color="primary"
           size="large" 
           startIcon={<SaveIcon />}>
-            Save 
+          Save
         </Button>
       </form>
       <br></br>
-      <ol id="toDo">
-        <li>Test item</li>
-      </ol>
+      {display(list)}
     </body>
     </div>
   );
