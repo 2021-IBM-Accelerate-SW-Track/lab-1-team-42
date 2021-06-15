e3c05516a204c5eb6efb3966e6db5a8dd498a648
CatMusty <jorji@mymail.mines.edu>
2021-06-14 19:56:12 -0500

    Revert "Test file altered, test input started"
    This reverts commit 8d01a23cb6ecb27ed1d8272143da73cc875cad73.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ function App() {
   return (
     <div className="App">
     <Header/>
-    <h1>To Do List</h1>
-    <p>
-      TEST INPUT AREA <br></br>
-      <input type="text" id="userInput" placeholder="To Do..."></input>
-    </p>
     </div>
   );
 }
