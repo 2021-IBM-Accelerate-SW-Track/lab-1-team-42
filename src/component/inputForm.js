438caad1b641f33be5995f1e58f886620baaa47b
CatMusty <jorji@mymail.mines.edu>
2021-06-14 19:55:14 -0500

    Revert "Deleted displayList module, folded into inputForm"
    This reverts commit a5e4819c8ffd3d203342d2bc9695949ec298aac1.

--- src/component/inputForm.js
+++ src/component/inputForm.js
@@ -1,21 +1,9 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Save from './saveButton';
+import displayItem from './displayList.js';
 
-let list = [];
-
-function displayItem(){
-    // HTML list is identified by ID from the document
-    let displayList = document.getElementById('toDo');
-    // For each entry in the array, a new HTML child is
-    // created and appended to the HTML list, meaning every
-    // time this is called the display list is overwritten
-    for(let i = 0; i < list.length; i++){
-        let item = document.createElement('li');
-        item.appendChild(document.createTextNode(list[i]));
-        displayList.append(item);
-    }
-}
+export let list = [];
 
 export function UserInput(){
     const [item, setItem] = useState('');
