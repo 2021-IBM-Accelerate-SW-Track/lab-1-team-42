33112b7783cfada3e6d15383e9ddabd641121475
CatMusty <jorji@mymail.mines.edu>
2021-06-14 19:56:05 -0500

    Revert "Background colour changed, inputField file created"
    This reverts commit 22a1df899a732cfc20ba0d2e9941473f31c92d74.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Header from "./component/header"
 import React from 'react'
 import './App.css';
-import inputField from './inputField.js';
-
+// Test comment
 function App() {
-  
   return (
     <div className="App">
     <Header/>
     <h1>To Do List</h1>
-    <body>
-      
-    </body>
+    <p>
+      TEST INPUT AREA <br></br>
+      <input type="text" id="userInput" placeholder="To Do..."></input>
+    </p>
     </div>
   );
 }
+
 export default App;
