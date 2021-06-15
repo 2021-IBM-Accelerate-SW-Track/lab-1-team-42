c8781655939dd352eb3fcc28ee9ef304381de3a8
CatMusty <jorji@mymail.mines.edu>
2021-06-14 19:55:19 -0500

    Revert "displayList and inputForm created"
    This reverts commit 81ffd5c918ce58537601dae68feb7e712f94bc72.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import Header from "./component/header"
-import React from 'react'
-import { UserInput } from "./component/inputForm";
+import React, {useState} from 'react'
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import Save from './component/saveButton.js';
 
 //test commment
 function App() {
   // Hook to store the state of the textField, and
   // a function to handle it.
+  const [item, setItem] = useState('');
+  
+  let list = [];
+  const handleItem = (e) =>{
+    e.preventDefault()
+
+    if(item && !list.includes(item)){
+      list.push(item);
+      displayItem();
+    }
+  }
+
+  // displayItem
+  //
+  // // Adds to and displays the given list in order
+  function displayItem(){
+    // HTML list is identified by ID from the document
+    let displayList = document.getElementById('toDo');
+    // For each entry in the array, a new HTML child is
+    // created and appended to the HTML list, meaning every
+    // time this is called the display list is overwritten
+    for(let i = 0; i < list.length; i++){
+      let item = document.createElement('li');
+      item.appendChild(document.createTextNode(list[i]));
+      displayList.append(item);
+    }
+  }
   
   return (
     <div className="App">
@@ -15,7 +43,15 @@ function App() {
     {/* Body contains text field and button within the same form,
     which then allows for submission upon click.*/}
     <body>
-      <UserInput/>
+      <form noValidate autoComplete="off" onSubmit={handleItem}> 
+        <TextField
+        onChange={(e) => setItem(e.target.value)} 
+        id="userInput" 
+        label=""
+        variant="filled"
+        />
+        <Save/>
+      </form>
       <br></br>
       <ol id="toDo">
       </ol>
